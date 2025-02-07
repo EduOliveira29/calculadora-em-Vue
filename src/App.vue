87682efcc47a1e673986cbf382b1c2e3ecfa5dc2
EduOliveira29 <realmadri123@yahoo.com.br>
2025-02-07 12:34:47 -0300

    finalizando projeto

--- src/App.vue
+++ src/App.vue
@@ -28,26 +28,28 @@ const resulSoma = () => {
 </script>
 
 <template>
-  <div class="container">
-    <header class="p-5 mt-4 mb-4 bg-light rounded-4">
-      <h1>calculadora</h1>
-      <p>O resultado é {{ resulSoma() }}</p>
+  <div class="container d-flex flex-column">
+    <header class="p-5 mt-4 mb-4 bg-light text-center">
+      <h1>Calculadora</h1>
+      <p class="fs-4">O resultado é <span class="fs-4"> {{ resulSoma() }}  </span></p>
     </header>
 
 
-    <form>
-        <div>
-            <input :value="estado.valorA" @change="evento => estado.valorA = evento.target.value" required type="text" placeholder="Digite seu numero aqui" >
-            <div class="col-md-2">
-          <select @change="evento => estado.filtro = evento.target.value">
+    <form class="text-center">
+      <div class="m-4">
+        <input class="form-control text-center" :value="estado.valorA" @change="evento => estado.valorA = evento.target.value" required type="text" placeholder="Digite seu numero aqui">
+      </div>
+      <div class="m-4">
+        <select class="text-center form-control" @change="evento => estado.filtro = evento.target.value">
             <option value="adicao">+</option>
             <option value="subtracao">-</option>
             <option value="multiplicacao">X</option>
             <option value="divisao">÷</option>
           </select>
-        </div>
-            <input :value="estado.valorB" @change="evento => estado.valorB = evento.target.value" required type="text" placeholder="Digite seu numero aqui">
-    </div>
+      </div>
+      <div class="m-4">
+        <input class="form-control text-center" :value="estado.valorB" @change="evento => estado.valorB = evento.target.value" required type="text" placeholder="Digite seu numero aqui">
+      </div>
     </form>
 </div>
 </template>
